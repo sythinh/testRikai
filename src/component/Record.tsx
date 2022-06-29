cc474c620fca1e2900a7d0eea0b52d3c34166740
sy.thinh <duythanh21@gmail.com>
2022-06-29 16:09:05 +0700

    create Page Record miss Chart component

---
+++ src/component/Record.tsx
@@ -0,0 +1,292 @@
+import React, { FC } from 'react';
+
+const Record: FC = () => {
+  return (
+    <div className="record">
+      <div className="container">
+        <div className="record-adds">
+          <div className="row">
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 item">
+              <div className="content">
+                <img src="src/image/body_image.png" alt="body-image" />
+                <div className="desc">
+                  <h1 className="title">BODY RECORD</h1>
+                  <p className="name">自分のカラダの記録</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 item">
+              <div className="content">
+                <img src="src/image/exercise.png" alt="exercise" />
+                <div className="desc">
+                  <h1 className="title">MY EXERCISE</h1>
+                  <p className="name">自分の運動の記録</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 item">
+              <div className="content">
+                <img src="src/image/diary_image.png" alt="diary_image" />
+                <div className="desc">
+                  <h1 className="title">MY DIARY</h1>
+                  <p className="name">自分の日記</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="chart"></div>
+        <div className="record-exercise">
+          <div className="title-item">
+            <h2>MY EXERCISE</h2>
+            <span>2021.05.21</span>
+          </div>
+          <ul className="row exercise-list">
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise ">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">25kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+            <li className="col-xs-5 col-sm-5 col-md-5 col-lg-5 ">
+              <div className="content-exercise">
+                <div className="info-exercise ">
+                  <p className="name">家事全般（立位・軽い）</p>
+                  <span className="value-cal">26kcal</span>
+                </div>
+                <div className="time">10 mins</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="my-diary">
+          <h1 className="title">MY EXERCISE</h1>
+          <div className="row">
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 item-diary">
+              <div className="time-diary">2021.05.21 23:25</div>
+              <p className="desc">
+                私の日記の記録が一部表示されます。
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Record;
